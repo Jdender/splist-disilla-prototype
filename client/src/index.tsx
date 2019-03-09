3accf25ee3c8a9304015dc207e38877bbdf4b5ea
Jdender~ <jdenderplays@gmail.com>
2019-03-09 18:14:23 -0500

    Add bulma

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import { render } from 'react-dom';
 import * as React from 'react';
 
 const App = () => (
-    <h1>Hello world</h1>
+    <h1 className='subtitle'>Hello world</h1>
 );
 
 render(<App/>, document.getElementById('app'));
