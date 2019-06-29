049bff421eb420e67b103e22b99c8c2194efd5ff
Jdender~ <jdenderplays@gmail.com>
2019-06-29 16:37:13 -0400

    Add a subscription to MessageView

--- client/src/messages/MessageView.tsx
+++ client/src/messages/MessageView.tsx
@@ -1,20 +1,43 @@
 import * as React from 'react';
 import gql from 'graphql-tag';
-import { useQuery } from 'react-apollo-hooks';
+import { useQuery, useSubscription } from 'react-apollo-hooks';
 import { IMessage } from 'typings';
 
 export const MessageView = () => {
 
-    const { data, error, loading } = useQuery(MESSAGE_QUERY, { 
-        variables: { 
-            skip: 0, 
-            take: 100,
-        },
+    const variables = {
+        skip: 0,
+        take: 100,
+    };
+
+    const { data, error, loading } = useQuery(MESSAGE_QUERY, { variables });
+
+    // TODO: Error handle subs by using "system messages"
+    useSubscription(MESSAGE_SUBSCRIPTION, {
+        onSubscriptionData: ({ client, subscriptionData }) => {
+
+            // Get queried messages
+            const cachedMessages = client.readQuery({
+                query: MESSAGE_QUERY,
+                variables,
+            });
+
+            // Update them
+            cachedMessages.messages.push(subscriptionData.data.messageSent);
+
+            // Put them back
+            client.writeQuery({
+                query: MESSAGE_QUERY,
+                data: cachedMessages,
+                variables,
+            });
+        }
     });
 
+    // TODO: Better error handling using "system messages"
     if (loading) return <div>Loading...</div>;
 
-    if (error) return <div>Error! {error.message}</div>;
+    if (error) return <div>Query Error! {error.message}</div>;
 
     return (
         <ul>
@@ -31,5 +54,14 @@ const MESSAGE_QUERY = gql`
             id
             content
         }
+    }
+`;
+
+const MESSAGE_SUBSCRIPTION = gql`
+    subscription {
+        messageSent {
+            id
+            content
+        } 
     }
 `;
